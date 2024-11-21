fix[poc-nvlty-crv]: compress spectrogram + vectorize & fix OSS computation
Refer to pg 19 & 23 for compression, pg 24 for OSS computation. Reference in [issue](https://github.com/AhmedAl-Hayali/GenreGuru/issues/258#issuecomment-2490048664).

src/poc/poc-test.py
@@ -30,9 +30,23 @@ def compute_novelty_curve(spectrogram):
         - Spectral flux is just the differences in the spectrogram from one time unit to the next
         - np.maximum(0, ...) takes care of the I(t, k) function 
     """
-    flux = np.zeros(spectrogram.shape[1])
-    for m in range(1, spectrogram.shape[1]):
-        flux[m] = np.sum(np.maximum(0, np.abs(spectrogram[:, m]) - np.abs(spectrogram[:, m - 1])))
+    N = spectrogram.shape[0]
+    fs = 11025
+
+    spectrogram = np.log(1+1000*spectrogram)
+    alpha = 1.76
+
+    flux = np.sum(
+        np.where(
+            (spectrogram[:, 1:] > alpha*spectrogram[:, :-1]) \
+                & ((30 <= np.arange(N)*fs/N) \
+                & (np.arange(N)*fs/N <= 720))[:, np.newaxis],
+            spectrogram[:, 1:] - spectrogram[:, :-1],
+            0
+        ),
+        axis=0 # sum across k
+    )
+
     return flux
 
 def compute_autocorrelation(signal):