645f4b4762e7161010ae308186828f3d00a28db9
bahsounm <76969859+bahsounm@users.noreply.github.com>
2025-02-02 13:29:36 -0500

    fix: updating features to use classes

--- src/featurizer/Instrumentalness_Featurizer.py
+++ src/featurizer/Instrumentalness_Featurizer.py
@@ -1,38 +1,52 @@
 import numpy as np
 
-"""module for the computation of the instrumentalness of the song.
-we do so by comparing the rms of the vocals to original, in order to avoid 
-extra computation on the non-vocal output. 
-"""
-
-def compute_instrumentalness(original_rms, vocal_rms, espilon = 1e-10):
+class InstrumentalnessComputation:
     """
-    computes instrumentalness [0~1] where a lower value means more vocals and a higher value means less vocals. 
-    instrumentalness is being defined as such:
-    Instrumentalness = 1 - (vocal_energy / original_energy)
-
-    somewhat attempts to account for spleeter's imperfect isolation (further exaberated by using mono input). 
-    assumption: all inputs have the same shape. BPM will not be recomputed, so should have same size
-    args:
-        original_rms (ndarray): feature vector of rms of original rms
-        vocal_rms (ndarray) : feature vector of rms of the vocal portion
-        espilon (float): prevents inf
-    returns: 
-        instrumentalness (2D ndarray): ndarray of instrumentalness value at each window
-        mean_instrumentalness (float64): returns the overall instrumentalness of the entire song
+    A class for computing instrumentalness of a song based on RMS values.
     """
-    instrumentalness = np.zeros((original_rms.shape[0], 1))
-    mean_instrumentalness = 0
 
-    for i in range(original_rms.shape[0]):
-        #compute db difference first.
-        db_diff = vocal_rms[i] - original_rms[i]
+    def __init__(self, epsilon=1e-10):
+        """
+        Initialize the InstrumentalnessComputation class.
 
-        #compute the ratio, use epsilon to avoid negative values:
-        energy_ratio = 10**(db_diff / 10)
-        current_instrumentalness = 1 - min(energy_ratio, 1-espilon)
-        instrumentalness[i] = current_instrumentalness
-        mean_instrumentalness += current_instrumentalness
+        Parameters:
+            epsilon (float): Small value to prevent division by zero or negative values.
+        """
+        self.epsilon = epsilon
+
+    """Module for the computation of the instrumentalness of the song.
+    We do so by comparing the RMS of the vocals to the original, in order to avoid 
+    extra computation on the non-vocal output.
+    """
+    def compute_instrumentalness(self, original_rms, vocal_rms):
+        """
+        Computes instrumentalness [0~1] where a lower value means more vocals and a higher value means less vocals.
+        Instrumentalness is being defined as:
+        Instrumentalness = 1 - (vocal_energy / original_energy)
+
+        Somewhat attempts to account for spleeter's imperfect isolation (further exacerbated by using mono input).
+        Assumption: All inputs have the same shape. BPM will not be recomputed, so should have the same size.
+
+        Parameters:
+            original_rms (ndarray): Feature vector of RMS of original audio.
+            vocal_rms (ndarray): Feature vector of RMS of the vocal portion.
         
-    mean_instrumentalness /= original_rms.shape[0]
-    return instrumentalness, mean_instrumentalness
+        Returns:
+            instrumentalness (ndarray): Instrumentalness value at each window.
+            mean_instrumentalness (float): Overall instrumentalness of the entire song.
+        """
+        instrumentalness = np.zeros((original_rms.shape[0], 1))
+        mean_instrumentalness = 0
+
+        for i in range(original_rms.shape[0]):
+            # Compute dB difference first.
+            db_diff = vocal_rms[i] - original_rms[i]
+
+            # Compute the ratio, use epsilon to avoid negative values:
+            energy_ratio = 10**(db_diff / 10)
+            current_instrumentalness = 1 - min(energy_ratio, 1 - self.epsilon)
+            instrumentalness[i] = current_instrumentalness
+            mean_instrumentalness += current_instrumentalness
+        
+        mean_instrumentalness /= original_rms.shape[0]
+        return instrumentalness, mean_instrumentalness
