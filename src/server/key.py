d8ca758de9b4ad144ae98569e2313f4a28974fc4
Matthew Baleanu <baleanum@mcmaster.ca>
2025-02-03 17:07:52 -0500

    dev[key]: proper styling to make key mapping a constant

--- src/server/key.py
+++ src/server/key.py
@@ -6,7 +6,7 @@ from scipy.stats import zscore
 class Key_Estimator:
     major_profile = np.asarray([6.35, 2.23, 3.48, 2.33, 4.38, 4.09, 2.52, 5.19, 2.39, 3.66, 2.29, 2.88])
     minor_profile = np.asarray([6.33, 2.68, 3.52, 5.38, 2.60, 3.53, 2.54, 4.75, 3.98, 2.69, 3.34, 3.17])
-    mapping = {0: "C", 1: "C#/Db", 2: "D", 3: "D#/Eb", 4: "E", 5: "F", 
+    KEY_MAPPING = {0: "C", 1: "C#/Db", 2: "D", 3: "D#/Eb", 4: "E", 5: "F", 
                    6: "F#/Gb", 7: "G", 8: "G#/Ab", 9: "A", 10: "A#/Bb", 11: "B"}
 
     def __init__(self, audio_path, sr=22050):
@@ -46,7 +46,7 @@ class Key_Estimator:
         pcd = self.get_pitch_class_distribution(notes)
         coefficients = self.get_coefficients(pcd)
         
-        major_key = f'{self.mapping[np.argmax(coefficients[0])]} Major'
-        minor_key = f'{self.mapping[np.argmax(coefficients[1])]} Minor'
+        major_key = f'{self.KEY_MAPPING[np.argmax(coefficients[0])]} Major'
+        minor_key = f'{self.KEY_MAPPING[np.argmax(coefficients[1])]} Minor'
 
         return major_key, minor_key
