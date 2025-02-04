24cb2ef4d11ac79cdfa355096f45b1068b97afcb
Ansel Chen <1chenans@gmail.com>
2025-02-03 19:01:57 -0500

    Fix[Instrumentalness-Featurizer]: Vectorized Function
    Co-Authored-By: bahsounm <76969859+bahsounm@users.noreply.github.com>

--- src/featurizer/Instrumentalness_Featurizer.py
+++ src/featurizer/Instrumentalness_Featurizer.py
@@ -14,48 +14,48 @@ class InstrumentalnessComputation:
         """
         self.epsilon = epsilon
 
+    # """Module for the computation of the instrumentalness of the song.
+    # We do so by comparing the RMS of the vocals to the original, in order to avoid 
+    # extra computation on the non-vocal output.
+    # """
+    # def compute_instrumentalness(self, original_rms, vocal_rms):
+    #     """
+    #     Computes instrumentalness [0~1] where a lower value means more vocals and a higher value means less vocals.
+    #     Instrumentalness is being defined as:
+    #     Instrumentalness = 1 - (vocal_energy / original_energy)
+
+    #     Somewhat attempts to account for spleeter's imperfect isolation (further exacerbated by using mono input).
+    #     Assumption: All inputs have the same shape. BPM will not be recomputed, so should have the same size.
+
+    #     Parameters:
+    #         original_rms (ndarray): Feature vector of RMS of original audio.
+    #         vocal_rms (ndarray): Feature vector of RMS of the vocal portion.
+        
+    #     Returns:
+    #         instrumentalness (ndarray): Instrumentalness value at each window.
+    #         mean_instrumentalness (float): Overall instrumentalness of the entire song.
+    #     """
+    #     instrumentalness = np.zeros((original_rms.shape[0], 1))
+    #     mean_instrumentalness = 0
+
+    #     for i in range(original_rms.shape[0]):
+    #         # Compute dB difference first.
+    #         db_diff = vocal_rms[i] - original_rms[i]
+
+    #         # Compute the ratio, use epsilon to avoid negative values:
+    #         energy_ratio = 10**(db_diff / 10)
+    #         current_instrumentalness = 1 - min(energy_ratio, 1 - self.epsilon)
+    #         instrumentalness[i] = current_instrumentalness
+    #         mean_instrumentalness += current_instrumentalness
+        
+    #     mean_instrumentalness /= original_rms.shape[0]
+    #     return instrumentalness, mean_instrumentalness
+
     """Module for the computation of the instrumentalness of the song.
     We do so by comparing the RMS of the vocals to the original, in order to avoid 
     extra computation on the non-vocal output.
     """
     def compute_instrumentalness(self, original_rms, vocal_rms):
-        """
-        Computes instrumentalness [0~1] where a lower value means more vocals and a higher value means less vocals.
-        Instrumentalness is being defined as:
-        Instrumentalness = 1 - (vocal_energy / original_energy)
-
-        Somewhat attempts to account for spleeter's imperfect isolation (further exacerbated by using mono input).
-        Assumption: All inputs have the same shape. BPM will not be recomputed, so should have the same size.
-
-        Parameters:
-            original_rms (ndarray): Feature vector of RMS of original audio.
-            vocal_rms (ndarray): Feature vector of RMS of the vocal portion.
-        
-        Returns:
-            instrumentalness (ndarray): Instrumentalness value at each window.
-            mean_instrumentalness (float): Overall instrumentalness of the entire song.
-        """
-        instrumentalness = np.zeros((original_rms.shape[0], 1))
-        mean_instrumentalness = 0
-
-        for i in range(original_rms.shape[0]):
-            # Compute dB difference first.
-            db_diff = vocal_rms[i] - original_rms[i]
-
-            # Compute the ratio, use epsilon to avoid negative values:
-            energy_ratio = 10**(db_diff / 10)
-            current_instrumentalness = 1 - min(energy_ratio, 1 - self.epsilon)
-            instrumentalness[i] = current_instrumentalness
-            mean_instrumentalness += current_instrumentalness
-        
-        mean_instrumentalness /= original_rms.shape[0]
-        return instrumentalness, mean_instrumentalness
-
-    """Module for the computation of the instrumentalness of the song.
-    We do so by comparing the RMS of the vocals to the original, in order to avoid 
-    extra computation on the non-vocal output.
-    """
-    def compute_instrumentalness_vec(self, original_rms, vocal_rms):
         """
         Computes instrumentalness [0~1] where a lower value means more vocals and a higher value means less vocals.
         Instrumentalness is being defined as:
