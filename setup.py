d2075969548ba860ff688795890af7f602d94923
Matthew Baleanu <baleanum@mcmaster.ca>
2025-03-03 21:48:21 -0500

    dev[vnv]: modify gitignore to exclude virtual environments

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup_info = dict(
     name = 'GenreGuru',
-    packages = [pkg for pkg in find_packages('src')]
+    packages = [pkg for pkg in find_packages(['src', 'test'])]
 )
 
 setup(**setup_info)
