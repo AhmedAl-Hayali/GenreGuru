f9b82aa4f7a68795ab65218f39d33cff52a5ecdd
jawadm1 <jawadm1@mcmaster.ca>
2025-03-27 13:12:00 -0400

    Fix: Bind Flask app to Render-provided PORT for successful deployment

--- src/url-store/server.py
+++ src/url-store/server.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+import os
 
 app = Flask(__name__)
 latest_url = None
@@ -12,3 +13,7 @@ def update_url():
 @app.route("/get-url", methods=["GET"])
 def get_url():
     return jsonify({"url": latest_url})
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
